aa8ec337b516f20fbd66f8ece38737304df8dcb9
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-01 16:42:25 +0530

    added a test for normal http

--- crawl.test.js
+++ crawl.test.js
@@ -22,3 +22,9 @@ test('normalizeURL capitals', () => {
     expect(actual).toEqual(expected)
 })
 
+test('normalizeURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
