83149d9135ca58185f665d5fa67763098b26319e
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-01 16:52:37 +0530

    added an empty check for HTML links

--- crawl.js
+++ crawl.js
@@ -1,3 +1,9 @@
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    return urls
+}
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +14,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML,
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -26,5 +26,21 @@ test('normalizeURL strip http', () => {
     const input = 'http://blog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/">
+                    Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = normalizeURL(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/"]
     expect(actual).toEqual(expected)
 })
